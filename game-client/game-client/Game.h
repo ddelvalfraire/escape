d1fe9b817bde1c69168e51d33ec82d065d201c90
David <ddelvalfraire@outlook.com>
2024-04-15 00:49:15 -0700

    Implement basic higher level Game and Program structure

---
+++ game-client/game-client/Game.h
@@ -0,0 +1,11 @@
+#ifndef GAME_H
+#define GAME_H
+
+class Game
+{
+public:
+
+	void update(float dt);
+
+};
+#endif // !GAME_H
--- game-client/game-client/main.cpp
+++ game-client/game-client/main.cpp
@@ -1,30 +1,9 @@
-// game-client.cpp : Defines the entry point for the application.
-//
-#include <SFML/Graphics.hpp>
-#include <box2d/box2d.h>
+#include "Program.h"
 
 int main(int, char* [])
 {
-    // testing a new commit and push from new device
-    b2BodyDef b;
-    auto&& window = sf::RenderWindow({ 1280, 720 }, "CMake Example");
-    auto&& circle = sf::CircleShape(50.f);
-    circle.setFillColor(sf::Color::Green);
-    circle.setPosition(640.f, 360.f);
-    circle.setOrigin(50.f, 50.f);
-
-    while (window.isOpen())
-    {
-        sf::Event event;
-        while (window.pollEvent(event))
-        {
-            if (event.type == sf::Event::Closed) window.close();
-        }
-
-        window.clear();
-        window.draw(circle);
-        window.display();
-    }
+    Program program;
+    program.run();
 
     return 0;
 }
